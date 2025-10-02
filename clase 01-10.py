13312dc632204eec896c8b7854d1b82971ba6098
Emmanuel <delorenzoemmanuel@gmail.com>
2025-10-01 22:55:25 -0300

    Update clase 01-10.py

--- clase 01-10.py
+++ clase 01-10.py
@@ -1,14 +1,11 @@
 import json
 base_datos = [
-    "Nombre,Apellido,Edad,Nacionalidad,estado_civil",
-    "Emiliano Alejandro,Billi,43,Argentino,Casado",
-    "Peter,Fonda,102",
-    "Mirtha,Legrand,98",
-    "Cesar,Billi,11",
-    "Emmanuel,Delorenzo,36",
-    "pepe,pepito,88",
-    "pepe,argento,54"
-]
+  "Nombre,Apellido,Edad",
+  "Emiliano Alejandro,Billi,43",
+  "Peter,Fonda,102",
+  "Mirtha,Legrand,98",
+  "Cesar,Billi,11"
+  ]
 
 keys = base_datos[0].split(",")
 
@@ -17,13 +14,13 @@ i = 1
 while i < len(base_datos):
   values = base_datos[i]
   values = values.split(",")
-  d = {
-    keys[0] : values[0],
-    keys[1] : values[1],
-    keys[2] : values[2]
-  }
+  d = {keys[0] : values[0],
+       keys[1] : values[1],
+       keys[2] : values[2]
+       }
   print(d)
   dict_db.append(d)
+  d
   i = i + 1
 print(dict_db)
 json_db = json.dumps(dict_db)
